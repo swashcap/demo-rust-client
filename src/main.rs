2028c8407d6bdbdee220f1cdac05044af2869121
Cory Reed <swashcap@gmail.com>
2019-08-09 12:17:47 -0700

    feat: make basic HTTP request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+extern crate env_logger;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let mut res = reqwest::get("http://httpbin.org/get")?;
+
+    println!("Response: {}", res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    Ok(())
 }
